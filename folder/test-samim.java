004a19ba3f1246fbdddb746f56dc0cbec266a976
CienKang <>
2023-06-23 06:56:23 +0000

    23/06/2023, 12:26:03

--- folder/test-samim.java
+++ folder/test-samim.java
@@ -1,5 +1,6 @@
 class ABC {
     public static void main(String args[]) {
-        System.out.println("Pro Coder Samim");
+        System.out.println("Pro Coder Samim Noob");
     }
 }
+\ No newline at end of file
